2629b0abd9cbb12be2e88c0ef707775342d1ff95
ErickOrtiz0298 <a13003218@alumnos.uady.mx>
2023-06-29 15:41:26 -0600

    Se agregaron los componentes hijos red y blue identificadores de GameCircle

--- src/Components/GameBoard.js
+++ src/Components/GameBoard.js
@@ -4,14 +4,30 @@ import GameCircle from "./GameCircle";
 const GameBoard = () =>{
     return (
         <div>
-            <GameCircle />
-            <GameCircle />
-            <GameCircle />
-            <GameCircle />
-            <GameCircle />
-            <GameCircle />
-            <GameCircle />
-            <GameCircle />
+            <GameCircle id={1}> 
+                Read
+            </GameCircle>
+            <GameCircle id={2}> 
+                Blue
+            </GameCircle>
+            <GameCircle id={3}> 
+                Read
+            </GameCircle>
+            <GameCircle id={4}> 
+                Blue
+            </GameCircle>
+            <GameCircle id={5}> 
+                Read
+            </GameCircle>
+            <GameCircle id={5}> 
+                Blue
+            </GameCircle>
+            <GameCircle id={7}> 
+                Read
+            </GameCircle>
+            <GameCircle id={8}> 
+                Blue
+            </GameCircle>
         </div>
 
     )
